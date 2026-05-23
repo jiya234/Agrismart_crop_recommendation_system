238ca75751f839450636f2e2499bc80500e68733
jeeyadhiman <jeeya.dhiman.2006@gmail.com>
2026-05-23 18:50:48 +0530

    sendgrid api added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,13 +16,12 @@ ALLOWED_HOSTS = ['*']
 
 # Email
 # Email - use console backend if SMTP fails
-EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY')
 
 INSTALLED_APPS = [
     'users',
